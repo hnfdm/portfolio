f2dcf81cdd0c07ef5c661f542e5aa2d46759f5cd
H <68972646+hnfdm@users.noreply.github.com>
2025-01-04 22:15:28 +0700

    Update Main.tsx

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -15,7 +15,7 @@ function Main() {
         </div>
         <div className="content">
           <div className="social_icons">
-            <a href="https://github.com/hanifdamarjati20" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+            <a href="https://github.com/hnfdm" target="_blank" rel="noreferrer"><GitHubIcon/></a>
             <a href="https://www.linkedin.com/in/hanifdamarjati20/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
 			<a href="mailto:hanif.damarjati20@gmail.com" target="_blank" rel="noreferrer"><EmailIcon/></a>
           </div>
