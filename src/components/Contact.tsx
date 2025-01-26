02acf7030416f211eb2aa5acffa7def7fcaa0e84
hanifdamarjati20 <hanif.damarjati20@gmail.com>
2025-01-27 01:32:37 +0700

    fix Contact page

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,8 +1,6 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
 import '../assets/styles/Contact.scss';
 // import emailjs from '@emailjs/browser';
-import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
 import SendIcon from '@mui/icons-material/Send';
 import TextField from '@mui/material/TextField';
 
@@ -16,8 +14,6 @@ function Contact() {
   const [emailError, setEmailError] = useState<boolean>(false);
   const [messageError, setMessageError] = useState<boolean>(false);
 
-  const form = useRef();
-
   const sendEmail = (e: any) => {
     e.preventDefault();
 
@@ -25,28 +21,28 @@ function Contact() {
     setEmailError(email === '');
     setMessageError(message === '');
 
-    /* Uncomment below if you want to enable the emailJS */
+    /* Uncomment below if you want to enable the emailJS 
 
-    // if (name !== '' && email !== '' && message !== '') {
-    //   var templateParams = {
-    //     name: name,
-    //     email: email,
-    //     message: message
-    //   };
+    if (name !== '' && email !== '' && message !== '') {
+      var templateParams = {
+        name: name,
+        email: email,
+        message: message
+      };
 
-    //   console.log(templateParams);
-    //   emailjs.send('service_id', 'template_id', templateParams, 'api_key').then(
-    //     (response) => {
-    //       console.log('SUCCESS!', response.status, response.text);
-    //     },
-    //     (error) => {
-    //       console.log('FAILED...', error);
-    //     },
-    //   );
-    //   setName('');
-    //   setEmail('');
-    //   setMessage('');
-    // }
+      console.log(templateParams);
+      emailjs.send('service_id', 'template_id', templateParams, 'api_key').then(
+        (response) => {
+          console.log('SUCCESS!', response.status, response.text);
+        },
+        (error) => {
+          console.log('FAILED...', error);
+        },
+      );
+      setName('');
+      setEmail('');
+      setMessage('');
+    } */
   };
 
   return (
@@ -55,13 +51,6 @@ function Contact() {
         <div className="contact_wrapper">
           <h1>Contact Me</h1>
           <p>Got a project waiting to be realized? Let's collaborate and make it happen!</p>
-          <Box
-            ref={form}
-            component="form"
-            noValidate
-            autoComplete="off"
-            className='contact-form'
-          >
             <div className='form-flex'>
               <TextField
                 required
@@ -72,11 +61,14 @@ function Contact() {
                 onChange={(e) => {setName(e.target.value);}}
                 error={nameError}
                 helperText={nameError ? "Please enter your name" : ""}
-				//InputLabelProps={style:{color: '#fff'}}
-				//sx={{input:{color:"pink"},style:{color:"pink"}}} 
-        sx={{
-          color: 'black',             // Warna teks
-        }}
+                sx={{
+                  textarea: {
+                    color: 'black', // Pastikan teks terlihat
+                  },
+                  '& .MuiOutlinedInput-root': {
+                    backgroundColor: 'white', // Pastikan background sesuai
+                  },
+                }}
               />
               <TextField
                 required
@@ -89,6 +81,14 @@ function Contact() {
                 }}
                 error={emailError}
                 helperText={emailError ? "Please enter your email or phone number" : ""}
+                sx={{
+                  textarea: {
+                    color: 'black', // Pastikan teks terlihat
+                  },
+                  '& .MuiOutlinedInput-root': {
+                    backgroundColor: 'white', // Pastikan background sesuai
+                  },
+                }}
               />
             </div>
             <TextField
@@ -105,11 +105,20 @@ function Contact() {
               }}
               error={messageError}
               helperText={messageError ? "Please enter the message" : ""}
+              sx={{
+                textarea: {
+                  color: 'black', // Pastikan teks terlihat
+                },
+                '& .MuiOutlinedInput-root': {
+                  backgroundColor: 'white', // Pastikan background sesuai
+                },
+              }}
             />
-            <Button variant="contained" endIcon={<SendIcon />} onClick={sendEmail}>
-              Send
-            </Button>
-          </Box>
+            <div className="button-container">
+              <button className="contact-button" onClick={sendEmail}>
+                SEND
+              </button>
+            </div>
         </div>
       </div>
     </div>
